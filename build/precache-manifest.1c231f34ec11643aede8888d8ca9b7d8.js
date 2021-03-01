57f239bfefe810d07682477cec21d0ed12ded9dd
KienDH <dustindinh.work@gmail.com>
2021-03-01 13:51:37 +0700

    add pagination

---
+++ build/precache-manifest.1c231f34ec11643aede8888d8ca9b7d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01760e7a0fde54ebce74d5d79fec4c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b027236f3ac483282160",
+    "url": "/static/css/2.0f8e233a.chunk.css"
+  },
+  {
+    "revision": "49b7d922dd070b2e1828",
+    "url": "/static/css/main.01cbfd1e.chunk.css"
+  },
+  {
+    "revision": "b027236f3ac483282160",
+    "url": "/static/js/2.a03d60a5.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.a03d60a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b7d922dd070b2e1828",
+    "url": "/static/js/main.143466e2.chunk.js"
+  },
+  {
+    "revision": "a0e3c214497d13a1723b",
+    "url": "/static/js/runtime-main.70190281.js"
+  }
+]);
