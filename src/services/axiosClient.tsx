a6231198eb44f062139d107be99a1bb38f83d3de
Quang Huy <kzyo5962@gmail.com>
2021-03-02 11:01:12 +0700

    CORS issue

--- src/services/axiosClient.tsx
+++ src/services/axiosClient.tsx
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'https://remotive.io/api/remote-jobs',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL:
+    //
+    'https://cors-anywhere.herokuapp.com/https://remotive.io/api/remote-jobs',
 });
 
 export default axiosClient;
