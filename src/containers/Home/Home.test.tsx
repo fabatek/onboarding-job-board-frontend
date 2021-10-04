33ba08dc2b7159d7f0219d1ebe9e00986044e13e
Minh Tien <cominhtien30@gmail.com>
2021-10-04 14:46:57 +0700

    create branch tiencm/job_board

--- src/containers/Home/Home.test.tsx
+++ src/containers/Home/Home.test.tsx
@@ -1,8 +1,8 @@
-import {render} from "@testing-library/react";
-import Home from "../Home/Home";
+import { render } from "@testing-library/react";
+import Home from "./index";
 import React from "react";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<Home />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
