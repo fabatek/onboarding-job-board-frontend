072f43572da0207134cca4fbcc3dde4d1ca20ea5
tiencm <cominhtien30@gmail.com>
2021-10-07 14:43:15 +0700

    feat(#t02): edit home.test.tsx

--- src/containers/Home/Home.test.tsx
+++ src/containers/Home/Home.test.tsx
@@ -1,15 +1,32 @@
-import { render } from "@testing-library/react";
-import Home from "./index";
+import { render, screen } from "@testing-library/react";
+import Jobs from "./jobs";
 import configureStore from "../Redux/Store";
 import { Provider } from "react-redux";
+import { SystemState } from "../Redux/type";
 
 const store = configureStore();
 import React from "react";
 
-test("renders learn react link", () => {
+test("renders learn react link", async () => {
+  let jobs: SystemState["jobs"] = [];
+  for (let index = 0; index < 100; index++) {
+    jobs.push({
+      name: "tester",
+      city: "",
+      district: "",
+      nation: "",
+      dateWork: "",
+      memberMin: 0,
+      memberMax: 0,
+      image: "",
+    });
+  }
+  //pass jobs into Home to test key city
   render(
     <Provider store={store}>
-      <Home />
+      <Jobs jobs={jobs} />
     </Provider>
   );
+  const element = screen.queryAllByText("tester"); //find text "tester"
+  expect(element).toHaveLength(100);
 });
