729ff4293fa69d68ff27f725f930549ac0da3de8
LAPTOP-0DFHV24K\khoan <nguyenduykhoa2601@gmail.com>
2021-10-27 12:20:56 +0700

    added two test : home, container

--- src/containers/Home/Container/ListJobs/ListsJobs.tsx
+++ src/containers/Home/Container/ListJobs/ListsJobs.tsx
@@ -44,7 +44,6 @@ const ListsJobs: FC = () => {
                                                     )
                                                 })
                                             }
-
                                         </div>
                                     </div>
                                 </li>
