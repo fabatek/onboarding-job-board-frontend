11ec08c78096ba4e021e4a66bda724e605bf724b
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-10-29 11:50:10 +0700

    update

--- src/containers/Home/Home.test.tsx
+++ src/containers/Home/Home.test.tsx
@@ -4,8 +4,8 @@ import React from "react";
 import Header from "../../components/Header";
 
 test('renders learn react link', () => {
-  // const { getByText } = render(<Header />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  render(<Home />)
+  const { getByText } = render(<Header />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+  
 });
