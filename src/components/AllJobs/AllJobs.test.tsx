80c12cc679c153b8069a017570931d1e691e2e1b
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 17:25:59 +0700

    Update AllJobs.test.tsx

--- src/components/AllJobs/AllJobs.test.tsx
+++ src/components/AllJobs/AllJobs.test.tsx
@@ -1,10 +1,22 @@
 import {render} from "@testing-library/react";
 import AllJobs from "./AllJobs";
 import React from "react";
+import {Provider} from 'react-redux';
+import store from '../../store'
 
-
-test('renders learn react link', () => {
-
-  render(<AllJobs />)
+describe("Step form reandering", () => {
+  it('it should render a step form`', () => {
+    const {container} = render(<Provider store={store}><AllJobs /></Provider>);
+    
+    expect(container).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
 });
 
+describe("Display jobs test", () => {
+  it('renders an `.jobscard`', () => {
+    const {container} = render(<Provider store={store}><AllJobs /></Provider>);
+    expect(container.querySelectorAll('.allJobs').length).toBe(0);
+  });
+  
+});
