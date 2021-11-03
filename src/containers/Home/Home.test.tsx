665ae542bffd7d066e88c7c72680f3661e6569d1
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 17:26:32 +0700

    Update Home.test.tsx

--- src/containers/Home/Home.test.tsx
+++ src/containers/Home/Home.test.tsx
@@ -2,10 +2,9 @@ import {render} from "@testing-library/react";
 import Home from "../Home/index";
 import React from "react";
 import Header from "../../components/Header";
-
+import { Provider } from 'react-redux';
+import store from '../../store'
 test('renders learn react link', () => {
-  // const { getByText } = render(<Header />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-  render(<Home />)
+  const { container } = render(<Provider store = {store}><Header /></Provider>);
+  expect(container).toBeInTheDocument();
 });
