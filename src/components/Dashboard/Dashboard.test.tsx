9c1d334c5b2d80584d359dba23cc1cf8aa74b38e
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 17:29:21 +0700

    Update Dashboard.test.tsx

--- src/components/Dashboard/Dashboard.test.tsx
+++ src/components/Dashboard/Dashboard.test.tsx
@@ -1,50 +1,34 @@
 import {render, fireEvent, screen} from "@testing-library/react";
 import Dashboard from "./Dashboard";
-import React from "react";
-import ReactPaginate from "react-paginate";
+import {Provider} from 'react-redux';
+import store from '../../store'
 describe("Step form reandering", () => {
   it('it should render a step form`', () => {
-    const {container} = render(<Dashboard />);
+    const {container} = render(<Provider store={store}><Dashboard /></Provider>);
     
     expect(container).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
   });
 });
 describe('<Dashboard />', () => {
     it('when rendering success', () => {
-      const {container} = render(<Dashboard />);
+      const {container} = render(<Provider store={store}><Dashboard /></Provider>);
       expect(container.querySelectorAll('.dashBoard').length).toBe(1);
     });
 });
-//Click Pagination
-describe("Step form interactive", () => {
-  const paginationActive = jest.fn;
-  it('should got to next or prev step', () => {
-    const {container, getByRole} = render(<Dashboard />);
-    const prev = container.querySelector('paginatonBttns') as HTMLElement;
-    fireEvent.click(prev)
-    expect(paginationActive).toHaveBeenCalledTimes(1);
-  });
-});
+
 //Search jobs
 describe('Input value', () => {
+  const setSearch = jest.fn((value) => {})
+  const { getByLabelText } = render(<Provider store={store}><Dashboard /></Provider>)
+  const searchInput = getByLabelText('Search') as HTMLInputElement;
   it('updates on change', () => {
-    const setSearch = jest.fn((value) => {})
     
-    const { getByLabelText } = render(<Dashboard />)
-
-    const searchInput = getByLabelText('Search') as HTMLInputElement;
-
     fireEvent.change(searchInput, { target: { value: 'test' } })
 
     expect(searchInput.value).toBe('test')
   })
+  
   it('value is null', () => {
-    const setSearch = jest.fn((value) => {})
-    
-    const { getByLabelText } = render(<Dashboard />)
-
-    const searchInput = getByLabelText('Search') as HTMLInputElement;
 
     fireEvent.change(searchInput, { target: { value: '' } })
 
