820bf034ab274e49d92a8af08c4bcfae6ecca2fa
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 14:43:55 +0700

    Update Dashboard.tsx

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,81 +1,97 @@
 import React,{useState, useEffect, FC} from 'react'
 import './Dashboard.scss'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import axios from 'axios';
-const options = ['Ho Chi Minh', 'Ha Noi', 'Da Nang', 'Can Tho'];
+import { Dropdown, Form } from 'react-bootstrap';
+import ReactPaginate from 'react-paginate';
+import { JobTotal } from '../TotalJobs/Job';
+import { useSelector, useDispatch } from 'react-redux';
+import {fetchJobs} from '../../data/api'
+interface RootState {
+    Job: [],
+    loading: boolean,
+    error: null
+  }
 const Dashboard : FC = () => {
-    const [jobs, setJobs] = useState([].slice(0, 50));
     const [pageNumber, setPageNumber] = useState(0);
-    const jobsPerPage = 10;
+    const jobsPerPage = 10; 
     const pagesVisited = pageNumber * jobsPerPage;
-    
-    
-    useEffect(() => {
-        axios.get('https://6176370c03178d00173daae3.mockapi.io/api/api')
-        .then(res => {
-            setJobs(res.data)
-        }).catch(err => {
-            console.log(err);
-        })
-    },[])
-    
+    const [searchTerm,setSearchTerm ] = useState("");
+    const jobs = useSelector((state: RootState) => state.Job.slice(0, 100));
+    const dispatch = useDispatch()
+     useEffect(() => {
+        dispatch(fetchJobs())
+        }, [dispatch])
+        
     //Pagination
     const pageCount = Math.ceil(jobs.length / jobsPerPage);
-    const changePage = ({ selected }:any) => {
-    setPageNumber(selected);
-  };
-
+    const changePage = ( {selected}:any ) => {
+            setPageNumber(selected);
+        };
+    
     return (
         <div className="dashBoard">
-            <h1>Có tất cả {jobs.length} IT Jobs For Chất Developers</h1>
+            <JobTotal/>
             <div className='search'>
                 <div className="search__form">
-                    <Box component="form" sx={{'& > :not(style)': { m: 1, width: '55ch' },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                    >
-                    <TextField id="outlined-basic" label="Search" variant="outlined" />
                     
-                    </Box>
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        options={options}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField {...params} label="City" />}
-                        />
+                    <Form.Control type="text" placeholder="Search..." onChange={(e)=>{ setSearchTerm(e.target.value)}} />
+                    
+                    <Dropdown>
+                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            City
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Ha Noi</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Ho Chi Minh</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Da Nang</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
             </div>
-                {jobs.slice(pagesVisited, pagesVisited + jobsPerPage)
-                    .map((jobs:any) => (       
-                        <div key={jobs.id} className="jobsCard">
-                            <div className="jobsCard__left">
-                                <img src={jobs.jobImg} alt="" />
+                {jobs.filter((val:any)=>{
+                    if(searchTerm===""){
+                        return val;
+                    } else if(
+                        val.jobName.toLowerCase().includes(searchTerm.toLowerCase()) 
+                    ){
+                        return val;
+                    }
+                    
+                    })
+                    .slice(pagesVisited, pagesVisited + jobsPerPage)
+                    .map((jobs:any) => {
+                        return (
+                            <div key={jobs.id} className="jobsCard">
+                                <div className="jobsCard__left">
+                                    <img src={jobs.jobImg} alt="" />
+                                </div>
+                                <div className="jobsCard__center">
+                                    <a href="" id="name">{jobs.jobName}</a>
+                                    <a href="" id="company">{jobs.jobCompany}</a>
+                                    <a href="" id="type">{jobs.jobType}</a>
+                                </div>
+                                <div className="jobsCard__right">
+                                    <p>{jobs.jobArea}</p>
+                                </div>
+                                
                             </div>
-                            <div className = "jobsCard__center">
-                                <a href="" id="name">{jobs.jobName}</a>
-                                <a href="" id="company">{jobs.jobCompany}</a>
-                                <a href="" id="type">{jobs.jobType}</a>
-                            </div>
-                            <div className="jobsCard__right">
-                                <p>{jobs.jobArea}</p>
-                            </div>
-                            
-                        </div>
-                    ))}
-            <div className="dashBoard__pagination">
-                <Stack spacing={2}>
-                    <Pagination count={10} />
-                </Stack>
-            </div>
 
-        </div>
-        
+                        );
+                    })}
+                    <ReactPaginate 
+                    previousLabel={"Previous"}
+                    nextLabel={"Next"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"paginationBttns"}
+                    previousLinkClassName={"previousBttn"}
+                    nextLinkClassName={"nextBttn"}
+                    disabledClassName={"paginationDisabled"}
+                    activeClassName={"paginationActive"}
+                    pageRangeDisplayed={10} marginPagesDisplayed={10}
+                    data-testid="pagination"
+                     />         
+        </div> 
     )
 }
 
