2a4959d4c1a592d54edcc26cfc9aac4f6e2d643c
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 14:43:17 +0700

    Display 100 jobs use redux

--- src/components/AllJobs/AllJobs.tsx
+++ src/components/AllJobs/AllJobs.tsx
@@ -1,38 +1,40 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, useEffect } from 'react'
 import './_AllJobs.scss'
-import axios from 'axios';
-import { jobs } from '../../types/jobsType'
 
+import { useSelector, useDispatch } from 'react-redux';
+import {fetchJobs} from '../../data/api'
+interface RootState {
+    Job: [],
+    loading: boolean,
+    error: null
+  }
 
 const AllJobs: FC = () => {
-    const [jobs, setJobs] = useState([]);
-    useEffect(() => {
-        axios.get('https://6176370c03178d00173daae3.mockapi.io/api/api')
-        .then(res => {
-            setJobs(res.data)
-        }).catch(err => {
-            console.log(err);
-        })
-    },[])
+    const jobs = useSelector((state: RootState) => state.Job);
+    const dispatch = useDispatch()
+     useEffect(() => {
+        dispatch(fetchJobs())
+        }, [dispatch])
+        
     return (
         <div>
-            <h2>All Jobs({jobs.length} Jobs)</h2>
+            
+            <h2>All Jobs</h2>
             {jobs.map((jobs:any) => (       
                 <div key={jobs.id} className="jobsCard">
                     <div className="jobsCard__left">
                         <img src={jobs.jobImg} alt="" />
                     </div>
                     <div className = "jobsCard__center">
-                        <a href="" id="name">{jobs.jobName}</a>
-                        <a href="" id="company">{jobs.jobCompany}</a>
-                        <a href="" id="type">{jobs.jobType}</a>
+                        <a href="" className="name">{jobs.jobName}</a>
+                        <a href="" className="company">{jobs.jobCompany}</a>
+                        <a href="" className="type">{jobs.jobType}</a>
                     </div>
                     <div className="jobsCard__right">
                         <p>{jobs.jobArea}</p>
                     </div>
-                    
                 </div>
-        ))}
+            ))}
         </div>
         
     )
