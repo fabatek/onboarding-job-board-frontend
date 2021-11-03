7099ca6d419e4b77b6a827181834e97b026f63ef
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 17:36:18 +0700

    Update Dashboard.test.tsx

--- src/components/Dashboard/Dashboard.test.tsx
+++ src/components/Dashboard/Dashboard.test.tsx
@@ -1,10 +1,36 @@
-import {render} from "@testing-library/react";
+import {render, fireEvent, screen} from "@testing-library/react";
 import Dashboard from "./Dashboard";
-import React from "react";
-
-
-test('renders learn react link', () => {
-
-  render(<Dashboard />)
+import {Provider} from 'react-redux';
+import store from '../../store'
+describe("Step form reandering", () => {
+  it('it should render a step form`', () => {
+    const {container} = render(<Provider store={store}><Dashboard /></Provider>);
+    
+    expect(container).toBeInTheDocument();
+  });
+});
+describe('<Dashboard />', () => {
+    it('when rendering success', () => {
+      const {container} = render(<Provider store={store}><Dashboard /></Provider>);
+      expect(container.querySelectorAll('.dashBoard').length).toBe(1);
+    });
 });
 
+//Search jobs
+describe('Input value', () => {
+  const { getByLabelText } = render(<Provider store={store}><Dashboard /></Provider>)
+  const searchInput = getByLabelText('Search') as HTMLInputElement;
+  it('updates on change', () => {
+    
+    fireEvent.change(searchInput, { target: { value: 'test' } })
+
+    expect(searchInput.value).toBe('test')
+  })
+  
+  it('value is null', () => {
+
+    fireEvent.change(searchInput, { target: { value: '' } })
+
+    expect(searchInput.value).toBe('')
+  })
+})
