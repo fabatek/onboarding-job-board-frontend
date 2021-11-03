7431026f7dbcb4c06b28a2eb136cd0f51dbcec27
Minh Thi Phùng <73086906+minhthiphung989@users.noreply.github.com>
2021-11-03 17:27:37 +0700

    Update Dashboard.test.tsx

--- src/components/Dashboard/Dashboard.test.tsx
+++ src/components/Dashboard/Dashboard.test.tsx
@@ -1,19 +1,37 @@
 import {render, fireEvent, screen} from "@testing-library/react";
 import Dashboard from "./Dashboard";
-import React from "react";
-import ReactPaginate from "react-paginate";
-
+import {Provider} from 'react-redux';
+import store from '../../store'
 describe("Step form reandering", () => {
   it('it should render a step form`', () => {
-    const {container} = render(<Dashboard />);
+    const {container} = render(<Provider store={store}><Dashboard /></Provider>);
     
     expect(container).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
   });
 });
 describe('<Dashboard />', () => {
     it('when rendering success', () => {
-      const {container} = render(<Dashboard />);
+      const {container} = render(<Provider store={store}><Dashboard /></Provider>);
       expect(container.querySelectorAll('.dashBoard').length).toBe(1);
     });
 });
+
+//Search jobs
+describe('Input value', () => {
+  const setSearch = jest.fn((value) => {})
+  const { getByLabelText } = render(<Provider store={store}><Dashboard /></Provider>)
+  const searchInput = getByLabelText('Search') as HTMLInputElement;
+  it('updates on change', () => {
+    
+    fireEvent.change(searchInput, { target: { value: 'test' } })
+
+    expect(searchInput.value).toBe('test')
+  })
+  
+  it('value is null', () => {
+
+    fireEvent.change(searchInput, { target: { value: '' } })
+
+    expect(searchInput.value).toBe('')
+  })
+})
