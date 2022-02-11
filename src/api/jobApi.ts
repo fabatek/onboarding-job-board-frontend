e61598fe01f923f14d7ba69fa61b4d53b8f645a4
truongtrungkien0999 <truongtrungkien0999@gmail.com>
2022-02-11 15:33:42 +0700

    update the first 100 jobs t02

--- src/api/jobApi.ts
+++ src/api/jobApi.ts
@@ -3,7 +3,9 @@ import { Job } from "../state/constants/jobConstant";
 
 export const fetchJobsData = async (name: string) => {
   const { data } = await axios.get<Job[]>(`https://6201d6f6b8735d00174cb605.mockapi.io/api/job`);
-  const jobs = data.filter((job: Job) => Number(job.id)<=100);
+  // const jobs = data.filter((job: Job) => Number(job.id)<=100);
+  const TOTAL_JOBS = 100;
+  const jobs = data.filter((job: Job , index :number) => index < TOTAL_JOBS);
   return jobs;
   
 }
