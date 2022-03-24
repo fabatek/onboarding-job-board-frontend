95a407dc5a98bb148f4b24c69cdcb379c7eec9ff
MinhPhuongK57 <minhphuongk57.coder.it@gmail.com>
2022-03-24 14:55:50 +0700

    feat(#t02): display the first 100 jobs

--- src/containers/JobComponent/JobComponent.tsx
+++ src/containers/JobComponent/JobComponent.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import { RootStateOrAny, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./jobcomponent.scss";
 
 const JobComponent: React.FC = () => {
-  return <div>JobComponent</div>;
+  const jobs = useSelector((state: RootStateOrAny) => state.allJobs.jobs);
+  const RenderListJobs = (): ReactElement => {
+    return (
+      <>
+        {jobs.map((job: any) => {
+          const { id, name, company, image, status } = job;
+          return (
+            <section className="col-md-3" key={id}>
+              <Link to={`job/${id}`}>
+                <div className="card mb-4 card-custom">
+                  <input
+                    type="image"
+                    src={image}
+                    width="120px"
+                    height="180px"
+                    className="card-img-top"
+                    alt={image}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title text-lg-center fs-4 fw-bold text-warning">
+                      {company}
+                    </h5>
+                    <p className="card-text mb-4 text-dark">{name}</p>
+                    <p className="card-text mb-4 text-black fw-bold">
+                      {status === 1 ? (
+                        <div className="text-danger">Tạm ngưng</div>
+                      ) : (
+                        <div className="text-success">Đang hoạt động</div>
+                      )}
+                    </p>
+                    <Link to={`job/${id}`} className="btn btn-primary w-100">
+                      See more
+                    </Link>
+                  </div>
+                </div>
+              </Link>
+            </section>
+          );
+        })}
+      </>
+    );
+  };
+  return (
+    <>
+      {jobs.length > 0 ? (
+        <RenderListJobs />
+      ) : (
+        //Using Loading.. with h1 ta because RenderListJobs => Asynchronous
+        <h1 className="fs-5">Loading...</h1>
+      )}
+    </>
+  );
 };
-
 export default JobComponent;
