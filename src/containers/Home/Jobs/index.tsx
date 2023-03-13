d6ae66c0bda928eaed2a532c02b78867ce6857e7
Trieu <trieut046@gmail.com>
2023-03-13 17:28:43 +0700

    feat(t01): render 100 jobs

--- src/containers/Home/Jobs/index.tsx
+++ src/containers/Home/Jobs/index.tsx
@@ -18,7 +18,7 @@ export default function Jobs() {
         setJobs(data);
     }, [])
     const renderJob = () => {
-        return currentJob?.map((item:any)=>{
+        return data?.map((item:any)=>{
             return(
                 <li className='list-group-item d-flex'>
                     <img src='https://itviec.com/rails/active_storage/representations/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBLzRvREE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--0c0d2b976b107ef51289f0361754cea78ab9313e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdCem9MWm05eWJXRjBTU0lJY0c1bkJqb0dSVlE2RTNKbGMybDZaVjloYm1SZmNHRmtXd2RwUm1sRyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--80ef1c6b35a79f9e028daec95b88475f53428fd7/floware-logo.png' />
