3df48468254cb41720807800ac65f61db94a2b45
Danh <vutiendanh.0101@gmail.com>
2023-03-15 12:08:33 +0700

    fix(t02): Add loading screen and test

--- src/containers/Home/index.test.tsx
+++ src/containers/Home/index.test.tsx
@@ -1,21 +1,57 @@
-import { getByDisplayValue, render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
+
+import "@testing-library/jest-dom";
 import Home from "../Home";
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "../../redux/configStore";
-import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
 test("renders welcome message", () => {
-  const { getByText } = render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
-      </Provider>
-    </React.StrictMode>
+  const { queryAllByTestId, queryByTestId } = render(
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
-  const linkElement = getByText('Có Việc Làm IT Cho Developer "Chất"');
+  // const linkElement = getByText(
+  //   /Lorem, ipsum dolor sit amet consectetur adipisicing elit.Unde adipisci deserunt amet eveniet delectus aliquid/i
+  // );
+  const title = queryByTestId("title");
+  const job = queryAllByTestId("job");
+  // const jobContent = queryAllByTestId("job__content");
+  // const contentImg = queryAllByTestId("content__image");
+  // const contentImageRandom = queryAllByTestId("content__image_random");
+  // const contentTag = queryAllByTestId("content-tag");
+  // const contentStatus = queryAllByTestId("content__status");
+  // const contentName = queryAllByTestId("content-name");
 
-  expect(linkElement).toBeInTheDocument();
+  expect(title).toHaveClass("title");
+  if (job.length > 0) {
+    expect(job).toHaveClass("job");
+  }
+
+  // expect(jobContent).toHaveClass("job__content");
+  // expect(contentImg).toHaveClass("content-image");
+  // expect(contentImageRandom).toHaveClass("content__image-random");
+  // expect(contentTag).toHaveClass("content-tag");
+  // expect(contentStatus).toHaveClass("content__status");
+  // expect(contentName).toHaveClass("content-name");
+  // expect(linkElement).toBeInTheDocument();
+});
+describe("Loading API", () => {
+  it("should show loading and change UI after 2 seconds", async () => {
+    const { getByTestId, queryAllByTestId } = render(
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
+    const job = queryAllByTestId("job");
+    const loading = getByTestId("loading");
+    expect(loading).toBeVisible();
+
+    await waitFor(() => getByTestId("loading"));
+    if (job.length > 0) {
+      expect(getByTestId("loading")).toHaveClass("job__list");
+    }
+  });
 });
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import LoadingOverlay from "react-loading-overlay-ts";
 import { useDispatch, useSelector } from "react-redux";
 
 import "./styles.scss";
 import { jobAPI } from "../../redux/reducer/JobReducer";
 import { DispatchType, RootState } from "../../redux/configStore";
+import BounceLoader from "react-spinners/BounceLoader";
 export interface Job {
   id: number;
   name: string;
@@ -35,33 +37,61 @@ function Home() {
     }
     setCount(count);
   };
-
+  function loadingContent() {
+    return jobs.length > 0 ? false : true;
+  }
   return (
     <div className="App">
-      <h1 style={{ textAlign: "left" }}>Nhà tuyển dụng hàng đầu</h1>
+      <h1 style={{ textAlign: "left" }} data-testid="title" className="title">
+        Nhà tuyển dụng hàng đầu
+      </h1>
       <h2>{count} Có Việc Làm IT Cho Developer "Chất"</h2>
-      <div
-        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      <LoadingOverlay
+        active={loadingContent()}
+        spinner={<BounceLoader className="loading" />}
+        text="Loading your content..."
       >
-        {jobs.map((job) => {
-          return (
-            <div className="job" key={job.id}>
-              <div className="job__content">
-                <div
-                  className={`content-tag job__content--${
-                    job.status ? "green" : "red"
-                  }`}
-                >
-                  <span className="content__status">
-                    {job.status ? "Available" : "Non-available"}
-                  </span>
+        <div className="job__list" data-testid="loading">
+          {jobs.map((job) => {
+            return (
+              <div className="job" data-testid="job" key={job.id}>
+                <div className="job__content" data-testid="job__content">
+                  <div className="content__image" data-testid="content__image">
+                    <img
+                      className="content__image-random"
+                      data-testid="content__image-random"
+                      src={`https://picsum.photos/${
+                        Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000
+                      }`}
+                      alt=""
+                    />
+                  </div>
+                  <div
+                    className={`content-tag job__content--${
+                      job.status ? "green" : "red"
+                    }`}
+                    data-testid="content-tag"
+                  >
+                    <span
+                      data-testid="content__status"
+                      className="content__status"
+                    >
+                      {job.status ? "Available" : "Non-available"}
+                    </span>
+                  </div>
+                  <h2 className="content-name" data-testid="content-name">
+                    {job.name}
+                  </h2>
+                  <p className="content-description">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Unde adipisci deserunt amet eveniet delectus aliquid.
+                  </p>
                 </div>
-                <h2 className="content-name">{job.name}</h2>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      </LoadingOverlay>
     </div>
   );
 }
