7cdc3aaafd42b16cbec44ad4664b75d8c85952aa
Danh <vutiendanh.0101@gmail.com>
2023-03-16 15:20:16 +0700

    feat(t04): search job by title

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -20,17 +20,20 @@ function Home() {
 
   const [count, setCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchInput, setSearchInput] = useState("");
+  const [currentJobs, setCurrentJobs] = useState<Job[]>(jobs);
   const jobsPerPage = 10;
-  const currentJobs = jobs.slice(
-    (currentPage - 1) * jobsPerPage,
-    currentPage * jobsPerPage
-  );
+
   const dispatch: DispatchType = useDispatch();
 
   useEffect(() => {
     dispatch(jobAPI());
     countAvailableJob();
-  });
+    handleSearch();
+    setCurrentJobs(
+      jobs.slice((currentPage - 1) * jobsPerPage, currentPage * jobsPerPage)
+    );
+  }, []);
 
   const countAvailableJob = () => {
     let arr = jobs.filter((job) => job.status);
@@ -47,12 +50,58 @@ function Home() {
   };
   const validCurrentPageNext = !!(currentPage >= 10);
   const validCurrentPagePrevious = !!(currentPage <= 0);
+
+  const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
+  const handleSearch = () => {
+    setCurrentJobs(currentJobs.filter((job) => job.name === searchInput));
+  };
+
   return (
     <div className="App">
-      <h1 style={{ textAlign: "left" }} data-testid="title" className="title">
+      <div className="header">
+        <img
+          src="https://itviec.com/assets/logo-itviec-4492a2f2577a15a0a1d55444c21c0fa55810822b3b189fc689b450fb62ce0b5b.png"
+          className="header__logo"
+          alt=""
+        />
+      </div>
+      <div className="search">
+        <div className="search__content">
+          <div className="job--available">
+            <h2>{count} Có Việc Làm IT Cho Developer "Chất"</h2>
+          </div>
+          <div className="search__bar">
+            <div className="search--icon">
+              <i className="fa fa-search"></i>
+            </div>
+            <input
+              type="text"
+              className="search__input"
+              placeholder="Search job by title"
+              onChange={handleSearchInput}
+            />
+            <span>
+              <button
+                className="btn__search"
+                onClick={() => {
+                  handleSearch();
+                }}
+              >
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+      <h1
+        style={{ textAlign: "left", marginLeft: "50px" }}
+        data-testid="title"
+        className="title"
+      >
         Nhà tuyển dụng hàng đầu
       </h1>
-      <h2>{count} Có Việc Làm IT Cho Developer "Chất"</h2>
       <div className="loading">
         <LoadingOverlay
           active={loadingContent}
@@ -119,22 +168,26 @@ function Home() {
               >
                 Previous
               </button>
-              {Array.from({ length: Math.ceil(jobs.length / jobsPerPage) }).map(
-                (_, index) => (
-                  <button
-                    data-testid={`btn__page`}
-                    className={
-                      currentPage === index + 1
-                        ? "btn__page btn__page--focus"
-                        : "btn__page "
-                    }
-                    key={index}
-                    onClick={() => handlePageChange(index + 1)}
-                  >
-                    {index + 1}
-                  </button>
-                )
-              )}
+              {Array.from({
+                length: Math.ceil(
+                  (currentJobs.length < jobs.length / 10
+                    ? currentJobs.length
+                    : jobs.length) / jobsPerPage
+                ),
+              }).map((_, index) => (
+                <button
+                  data-testid={`btn__page`}
+                  className={
+                    currentPage === index + 1
+                      ? "btn__page btn__page--focus"
+                      : "btn__page "
+                  }
+                  key={index}
+                  onClick={() => handlePageChange(index + 1)}
+                >
+                  {index + 1}
+                </button>
+              ))}
               <button
                 onClick={() => {
                   changePage(1);
