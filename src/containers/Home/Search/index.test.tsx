45349092b0e21e1b22fbbf2a2d22cb367cc6a3ad
Trieu <trieut046@gmail.com>
2023-03-17 09:26:41 +0700

    feat(t01): fix code review

--- src/containers/Home/Search/index.test.tsx
+++ src/containers/Home/Search/index.test.tsx
@@ -1,20 +1,10 @@
 import {render,screen} from "@testing-library/react";
 import Search from "./index";
-import axios from "axios";
 test('renders inital document', () => {
     render(<Search/>);
-    const jobEle = screen.getByTestId("jobTest");
-    expect(jobEle).toBeInTheDocument();   
+    const jobElement = screen.getByTestId("jobTest");
+    expect(jobElement).toBeInTheDocument();   
 });
-test('render 100 jobs', async ()=>{
-    let data =0;
-    await axios.get(`${process.env.REACT_APP_API_URL}/products`)
-       .then(res=>{
-        data = res.data.length;
-       })
-       .catch(error => console.log(error));
-    expect(data).toBe(100);
-})
 test('selection default is All Cities',  ()=>{
     render(<Search/>)
     const seclectEle = screen.getByDisplayValue("All Cities"); 
